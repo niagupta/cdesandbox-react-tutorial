9c6869fd140f91718173ab792222c3f8fae5b938
niagupta <96285597+niagupta@users.noreply.github.com>
2022-01-31 12:55:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Arithmetic from "./components/Arithmetic";
+import ReduxDemo from "./components/reduxDemo";
+import "./styles/styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Arithmetic />
+      <h1>Redux Demo</h1>
+      <ReduxDemo />
+    </div>
+  );
+}
